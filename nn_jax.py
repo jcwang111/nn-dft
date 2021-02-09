cd20a178c0d03d08a8d5b904e8d95cf659540215
jcwang111 <23427258+jcwang111@users.noreply.github.com>
2021-02-09 14:04:00 -0800

    some test

--- nn_jax.py
+++ nn_jax.py
@@ -42,50 +42,54 @@ def grad_descent_update(params, batch_x, batch_y, eta):
     return [(w - eta*dw, b - eta*db) for (w,b),(dw,db) in zip(params, grads)]
 
 #Retrieve data
-data = datasets.Dataset(path='data/h2/', num_grids=513)
+data = datasets.Dataset(path='data/H3/')
 densities = data.densities
 total_energies = data.total_energies
 
-train_distances = [128, 384]
+train_distances = [200, 344]
 train_mask = jnp.isin(data.distances_x100, train_distances) #Only train at 1.28 and 3.84
 validation_distances = [296]
 validation_mask = jnp.isin(data.distances_x100, validation_distances) #Only train at 1.28 and 3.84
 
+assert(densities[train_mask].shape[0] == len(train_distances))
+
 #Hyperparameters
-init_key = random.PRNGKey(5) #parameter seed
+orig_init_key = random.PRNGKey(34000) #parameter seed
 layers = (513, 10, 10, 10, 1) #network structure
-eta = 0.5 #training rate
+#eta = 0.5 #training rate
 act_func = silu #activation function
-epochs = 800 #number of training epochs
+epochs = 1000 #number of training epochs
 
 #Cross_validate
-#seeds = [0, 1, 2, 3, 4, 5]
-#etas = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
+seeds = random.split(orig_init_key, 50)
+etas = [0.001, 0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0]
 
-#best_stats = {}
-#best_valid_error = jnp.inf
-#for eta in etas:
-#    for seed in seeds:
-#       init_key = random.PRNGKey(seed)
-params = init_network_params(layers, init_key)
-for i in range(epochs):
-    params = grad_descent_update(params, densities[train_mask,:], total_energies[train_mask], eta)
-    if i%200==0:
-        print('Training Cost:', L2_cost(params, densities[train_mask,:], total_energies[train_mask]))
-print('Training Cost:', L2_cost(params, densities[train_mask,:], total_energies[train_mask]))
-print('Validation Cost:', L2_cost(params, densities[validation_mask,:], total_energies[validation_mask]))
-#train_cost = L2_cost(params, densities[train_mask,:], total_energies[train_mask])
-#valid_cost = L2_cost(params, densities[validation_mask,:], total_energies[validation_mask])
-#if valid_cost < best_valid_error:
-#    best_valid_error = valid_cost
-#    best_stats = {'seed':seed, 'eta':eta, 'train_cost':train_cost, 'valid_cost':valid_cost}
+best_stats = {}
+best_params = None
+best_valid_error = jnp.inf
+for eta in etas:
+    for init_key in seeds:
+        params = init_network_params(layers, init_key)
+        for i in range(epochs):
+            params = grad_descent_update(params, densities[train_mask,:], total_energies[train_mask], eta)
+            #if i%200==0:
+                #print('Training Cost:', L2_cost(params, densities[train_mask,:], total_energies[train_mask]))
+        #print('Training Cost:', L2_cost(params, densities[train_mask,:], total_energies[train_mask]))
+        #print('Validation Cost:', L2_cost(params, densities[validation_mask,:], total_energies[validation_mask]))
+        train_cost = L2_cost(params, densities[train_mask,:], total_energies[train_mask])
+        valid_cost = L2_cost(params, densities[validation_mask,:], total_energies[validation_mask])
+        if valid_cost < best_valid_error:
+            best_valid_error = valid_cost
+            best_stats = {'seed':init_key, 'eta':eta, 'train_cost':train_cost, 'valid_cost':valid_cost}
+            best_params = params
 
-#print("------------------------")
-#print("Best hyperparameters:")
-#for k, v in best_stats.items():
-#    print(k,':',v)
+print("------------------------")
+print("Best hyperparameters:")
+for k, v in best_stats.items():
+    print(k,':',v)
+print(train_distances)
 
-result_energies = batch_predict(params, densities)
+result_energies = batch_predict(best_params, densities)
 
 #Nuclear-nuclear repulsion energy, from the 1D exponential interaction
 nuclear_energy = utils.get_nuclear_interaction_energy_batch(
@@ -95,7 +99,7 @@ nuclear_energy = utils.get_nuclear_interaction_energy_batch(
 
 
 plt.plot(data.distances, nuclear_energy+total_energies, linestyle='dashed', color='black', label='Exact')
-plt.plot(data.distances, nuclear_energy+result_energies, color='green', label='Network trained on samples D=1.28 and D=3.84')
+plt.plot(data.distances, nuclear_energy+result_energies, color='green',label='Network trained on total energy')
 plt.plot(data.distances[train_mask], (nuclear_energy+total_energies)[train_mask], marker='D', linestyle='None')
 plt.plot(data.distances[validation_mask], (nuclear_energy+total_energies)[validation_mask], marker='^', linestyle='None')
 plt.plot()
@@ -103,4 +107,5 @@ plt.xlabel('Distance')
 plt.ylabel('$E + E_{nn}$')
 plt.legend()
 
+#plt.savefig('h3_5pts.png')
 plt.show()
