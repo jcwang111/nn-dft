80c92aed28afcaf4d5bc3c1a6d71893e1d491d1d
jcwang111 <23427258+jcwang111@users.noreply.github.com>
2021-02-09 20:53:07 -0800

    optimal valid cost

--- pureML_jax_nn.py
+++ pureML_jax_nn.py
@@ -7,23 +7,23 @@ from jax import random
 from jax.nn import sigmoid, relu, softplus, silu, elu
 from jax.scipy import optimize
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 from jax_dft import datasets
 from jax_dft import utils
 from jax_dft import np_utils
+import scipy
 
 from pureML_layers import *
 
 #Retrieve data
-data = datasets.Dataset(path='data/h2/', num_grids=513)
+data = datasets.Dataset(path='data/h2', num_grids=513)
 densities = data.densities
 total_energies = data.total_energies
 grids = data.grids
 
 def nn_functions(*layer_gens):
     """Takes tuples of (init_function, predict_function)
-    and returns the init function and list of predictions"""
+    and puts them together"""
     num_layers = len(layer_gens)
     init_funcs = [layer[0] for layer in layer_gens]
     predict_funcs = [layer[1] for layer in layer_gens]
@@ -71,34 +71,43 @@ validation_mask = jnp.isin(data.distances_x100, validation_distances) #Only trai
 
 train_densities = densities[train_mask,:]
 train_energies = total_energies[train_mask]
-#Hyperparameters: seed=1 and eta=0.1 are optimal from cross-validation
-seed = 1 #parameter seed
-eta = 0.1 #training rate
-epochs = 1000 #number of training epochs
+valid_densities = densities[validation_mask,:]
+valid_energies = total_energies[validation_mask]
+
+#Hyperparameters
+#init_key = random.PRNGKey(1) #parameter seed
+#eta = 0.1 #training rate
+epochs = 20 #number of training epochs
 
 #Cross_validate
-#seeds = range(25)
+seeds = random.split(random.PRNGKey(68000), 50)
 #etas = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0]
 
-#best_stats = {}
-#best_valid_error = jnp.inf
+best_stats = {}
+best_valid_error = [np.inf]
+best_valid_params = {'params':None, 'seed':None, 'valid_cost':None, 'calls': 0}
 #for eta in etas:
-#    for seed in seeds:
-init_key = random.PRNGKey(seed)
+#for init_key in seeds:
+init_key = np.array([3483222781, 1411702757], dtype=np.uint32)
 params = init_params(init_key)
-'''for i in range(epochs):
-    params = grad_descent_update(params, densities[train_mask,:], total_energies[train_mask], eta)
-    if i%10==0:
-        print('Training Cost:', MSE_Loss(params, densities[train_mask,:], total_energies[train_mask]))'''
-
 spec, flatten_init_params = np_utils.flatten(params) 
 grad_MSE = grad(MSE_Loss)
 
+calls = [0]
 def flatten_fn_grad(flatten_params):
-    params = np_utils.unflatten(spec, flatten_params)
-    return MSE_Loss(params, train_densities, train_energies), np_utils.flatten(grad_MSE(params, train_densities, train_energies))[1]
+    unflat_params = np_utils.unflatten(spec, flatten_params)
+    valid_loss = MSE_Loss(unflat_params, valid_densities, valid_energies)
+    calls[0] += 1
+    if valid_loss < best_valid_error[0]:
+        best_valid_error[0] = valid_loss
+        best_valid_params['params'] = unflat_params
+        best_valid_params['seed'] = init_key
+        best_valid_params['valid_cost'] = valid_loss
+        best_valid_params['calls'] = calls[0]
 
-final_params, training_cost, info = scipy.optimize.fmin_l_bfgs_b(
+    return MSE_Loss(unflat_params, train_densities, train_energies), np_utils.flatten(grad_MSE(unflat_params, train_densities, train_energies))[1]
+
+final_params, train_cost, info = scipy.optimize.fmin_l_bfgs_b(
     flatten_fn_grad,
     x0=np.array(flatten_init_params),
     # Maximum number of function evaluations.
@@ -108,21 +117,23 @@ final_params, training_cost, info = scipy.optimize.fmin_l_bfgs_b(
     pgtol=1e-14)
 
 params = np_utils.unflatten(spec, final_params)
-print(training_cost)
 
-#train_cost = MSE_Loss(params, densities[train_mask,:], total_energies[train_mask])
-#valid_cost = MSE_Loss(params, densities[validation_mask,:], total_energies[validation_mask])
-#if valid_cost < best_valid_error:
-#    best_valid_error = valid_cost
-#    best_stats = {'seed':seed, 'eta':eta, 'train_cost':train_cost, 'valid_cost':valid_cost}
+print('Training Cost:', MSE_Loss(params, densities[train_mask,:], total_energies[train_mask]))
+print('Validation Cost:', MSE_Loss(params, densities[validation_mask,:], total_energies[validation_mask]))
+'''valid_cost = MSE_Loss(params, densities[validation_mask,:], total_energies[validation_mask])
+    if valid_cost < best_valid_error:
+        best_valid_error = valid_cost
+        best_stats = {'seed':init_key, 'info':info, 'train_cost':train_cost, 'valid_cost':valid_cost}'''
 
 #print("------------------------")
 #print("Best hyperparameters:")
 #for k, v in best_stats.items():
 #    print(k,':',v)
 
-print('Training Cost:', MSE_Loss(params, densities[train_mask,:], total_energies[train_mask]))
-print('Validation Cost:', MSE_Loss(params, densities[validation_mask,:], total_energies[validation_mask]))
+print(best_valid_params)
+params = best_valid_params['params']
+print('Training Cost:', MSE_Loss(best_valid_params['params'], densities[train_mask,:], total_energies[train_mask]))
+print('Validation Cost:', MSE_Loss(best_valid_params['params'], densities[validation_mask,:], total_energies[validation_mask]))
 result_energies = predict(params, densities)
 
 #Nuclear-nuclear repulsion energy, from the 1D exponential interaction
@@ -133,12 +144,12 @@ nuclear_energy = utils.get_nuclear_interaction_energy_batch(
 
 
 plt.plot(data.distances, nuclear_energy+total_energies, linestyle='dashed', color='black', label='Exact')
-plt.plot(data.distances, nuclear_energy+result_energies, color='purple', label='Convolutional network trained on D=1.28 and 3.84')
+plt.plot(data.distances, nuclear_energy+result_energies, color='purple', label='Training point with lowest validation cost')
 plt.plot(data.distances[train_mask], (nuclear_energy+total_energies)[train_mask], marker='D', linestyle='None')
 plt.plot(data.distances[validation_mask], (nuclear_energy+total_energies)[validation_mask], marker='^', linestyle='None')
 
 plt.xlabel('Distance')
 plt.ylabel('$E + E_{nn}$')
-plt.legend()
+#plt.legend()
 
 plt.show()
